31c02a1c29b071ff6bc671a7296821549a881e6d
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-10-07 22:39:50 -0500

    Working on script

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
-var searchForm = document.querySelector('#search-form');
-var findCityWeather =document.getElementById('#city-weather')
+var apiKey = "f23c92af2b429cef241bcb96ff0315fb";
+var currentWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q="
+var forecastWeatherApi = "https://api.openweathermap.org/data/2.5/forecast?q="
+var searchCityWeather = document.getElementById('search-form')
+var unit = "units=imperial";
 
-function searchBar(event) {
-    event.preventDefault();
 
-    var searchInput = document.querySelector('#search-input').value;
+//The city name and the date
+function getCityWeather(findCityName) {
+  var apiUrl = currentWeatherApi + findCityName + "&" + apiKey + "&" + unit;
+  fetch(apiUrl).then(function (response) {
+    if(response.ok) {
+      return response.json().then(function (response) {
+        document.getElementById('nameOfTheCity').innerHTML(response.name); 
 
-    if (!searchInput) {
-        console.error('You need a search input value!');
-        return;
-      }
-      var queryString = './search-results.html?q=' + searchInput;
+        var date= dayjs('M/D/YYYY')
+        document.getElementById('todaysDate').innerHTML(date);
+      })
+    }  else {
+      alert("Please provide a valid city name.");
+    }  
 
-      location.assign(queryString);
+  })
 }
-
-searchForm.addEventListener('search', searchBar);
-findCityWeather.addEventListener ('click, requestWeather);
